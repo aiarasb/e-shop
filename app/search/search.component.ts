e9ef0fdf6c1fd06535f073d864c70a1509c1124d
Aivaras Baranauskas <aivaras.baranauskas@hometogo.com>
2016-12-14 21:53:08 +0200

    pagination added to search

--- app/search/search.component.ts
+++ app/search/search.component.ts
@@ -4,6 +4,7 @@ import {Router} from "@angular/router";
 import {ProductService} from "../services/product.service";
 import {CategoryService} from "../services/category.service";
 import {Category} from "../categories/category";
+import {PagerService} from "../services/pager.service";
 
 @Component({
     moduleId: module.id,
@@ -14,12 +15,16 @@ import {Category} from "../categories/category";
 
 export class SearchComponent {
     products: Product[];
-    categories: Category[];
     results: Product[];
+    pagedResults: Product[];
+    categories: Category[];
     searchField: string;
+    pager: any = {};
+    productsPerPage: number = 10;
 
     constructor(private router: Router,
                 private categoryService: CategoryService,
+                private pagerService: PagerService,
                 private productService: ProductService) {
     }
 
@@ -31,6 +36,18 @@ export class SearchComponent {
         this.productService.getProducts().subscribe(products => this.products = products);
     }
 
+    setPage(page: number): void {
+        if (page < 1 || page > this.pager.totalPages) {
+            return;
+        }
+        if (this.results.length > 0) {
+            this.pager = this.pagerService.getPager(this.results.length, page, this.productsPerPage);
+            this.pagedResults = this.results.slice(this.pager.startIndex, this.pager.endIndex + 1);
+        } else {
+            this.pagedResults = [];
+        }
+    }
+
     doSearch(): void {
         if (this.searchField) {
             let re = new RegExp(this.searchField);
@@ -38,8 +55,9 @@ export class SearchComponent {
                 return re.test(value.name);
             });
         } else {
-            this.results = null;
+            this.results = this.products;
         }
+        this.setPage(1);
     }
 
     ngOnInit(): void {
