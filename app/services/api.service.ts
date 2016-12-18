89f042927c28d2300c7a733890a8e6c4f5bfd81c
avokadas <ignas.paplauskas@gmail.com>
2016-12-18 20:27:49 +0200

    Implement front-end login & resgister functionality

--- app/services/api.service.ts
+++ app/services/api.service.ts
@@ -14,18 +14,22 @@ export class ApiService {
 
     constructor(private http: Http) {}
 
-    login(username, password) {
+    login(username, password, onSuccess) {
         return this.http
-            .post(this.apiUrl + '/user/authenticate', JSON.stringify({username: username, password: password}), {headers: this.requestHeaders})
+            .post(this.apiUrl + '/user/login', JSON.stringify({username: username, password: password}), {headers: this.requestHeaders})
             .toPromise()
             .then((res) => {
-                console.log('weh');
+                console.log(res._body);
+                onSuccess(JSON.parse(res._body));
             })
-            .catch(this.handleError);
     }
 
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
+    register(username, password, onSuccess) {
+        return this.http
+            .post(this.apiUrl + '/user/create', JSON.stringify({username: username, password: password}), {headers: this.requestHeaders})
+            .toPromise()
+            .then((res) => {
+                onSuccess(JSON.parse(res._body));
+            })
     }
 }
